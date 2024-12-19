3ad63042495e3bdad67d4be5f78d975f68dfab09
jonadableite <jonadab.leite@gmail.com>
2024-12-19 19:16:25 -0300

    sidebar 1.0.2

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,26 +1,15 @@
 import SidebarComponent from "@/components/SidebarComponent";
 import { DropdownMenu } from "@/components/ui/dropdown-menu";
-import { SidebarProvider, useSidebar } from "@/components/ui/sidebar";
 import type React from "react";
 
 const LayoutSidebar: React.FC = ({ children }) => {
-	const { open } = useSidebar();
-
 	return (
-		<SidebarProvider>
-			<div className="flex">
-				<DropdownMenu>
-					<SidebarComponent />
-				</DropdownMenu>
-				<main
-					className={`flex-1 transition-all duration-300 ${
-						open ? "ml-60" : "ml-20"
-					}`}
-				>
-					{children}
-				</main>
-			</div>
-		</SidebarProvider>
+		<div className="flex">
+			<DropdownMenu>
+				<SidebarComponent />
+			</DropdownMenu>
+			<main className="flex-1 transition-all duration-300">{children}</main>
+		</div>
 	);
 };
 
