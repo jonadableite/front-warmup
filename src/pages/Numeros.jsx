724118a0ce3dea7fb6b9be000b9232de7307a403
jonadabLeite <jonadab.leite@gmail.com>
2025-04-23 22:32:30 -0300

    env

--- src/pages/Numeros.jsx
+++ src/pages/Numeros.jsx
@@ -9,7 +9,7 @@ import { Toaster, toast } from "react-hot-toast";
 import { FaWhatsapp } from "react-icons/fa";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:9000";
+  import.meta.env.VITE_API_BASE_URL || "https://aquecerapi.whatlead.com.br";
 const API_URL = "https://evo.whatlead.com.br";
 const API_KEY = "429683C4C977415CAAFCCE10F7D57E11";
 
